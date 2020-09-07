48caaa45ad38bde648d5b34ff884cf1ddbd1f5d7
Igor <GI_Something@protonmail.com>
2020-09-07 19:24:13 +0300

    feat: Add support for comparing regex-string pairs of different lengths

--- regex/regex.py
+++ regex/regex.py
@@ -1,10 +1,17 @@
 def match(regex, string):
-    if regex.__len__() > string.__len__():
-        return False
-    for i in range(regex.__len__()):
-        if regex[i] != string[i] and regex[i] != ".":
+    if not regex:
+        return True
+    for s_i in range(string.__len__()):
+        if regex.__len__() > string.__len__() - s_i:
             return False
-    return True
+        r_i = 0
+        while r_i < regex.__len__():
+            if regex[r_i] != string[s_i + r_i] and regex[r_i] != ".":
+                break
+            r_i += 1
+        if r_i == regex.__len__():
+            return True
+    return False
 
 
 print(match(*input().split("|")))
