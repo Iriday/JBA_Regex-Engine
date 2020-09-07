6129f72ee30fc184284b7f27314673e638bb8e23
Igor <GI_Something@protonmail.com>
2020-09-07 18:08:40 +0300

    feat: Add possibility to match 2 equal length strings(regex, string)

--- regex/regex.py
+++ regex/regex.py
@@ -1,13 +1,10 @@
 def match(regex, string):
-    if regex.__len__() == 0:
-        return True
-    if regex == string:
-        return True
-    if string.__len__() == 0:
+    if regex.__len__() > string.__len__():
         return False
-    if regex == ".":
-        return True
-    return False
+    for i in range(regex.__len__()):
+        if regex[i] != string[i] and regex[i] != ".":
+            return False
+    return True
 
 
 print(match(*input().split("|")))
